8de835837798e523abaebcaa727e24814b043322
linola <vasilevsergei995@gmail.com>
2024-10-13 01:19:17 +0300

    ver0.3 рабочая+-

--- source/func exit.py
+++ source/func exit.py
@@ -1,10 +1,10 @@
 
 
-lobby_reg = {'LBDEF':['user11','user12','user22'],'LB4410':['user21','user22']}
+"""lobby_reg = {'LBDEF':['user11','user12','user22'],'LB4410':['user21','user22']}
 name = 'user22'
-"""for i,j in lobby_reg.items():
+for i,j in lobby_reg.items():
     if name in j:
-        j.remove(name)"""
+        j.remove(name)
 GM = 'user11'
 lob = "LBDEF"
 if lob in lobby_reg.keys():
@@ -12,4 +12,20 @@ if lob in lobby_reg.keys():
         print('Вы можете отправить сообщение')
 
     else:
-        print("У вас недостаточно прав")
+        print("У вас недостаточно прав")"""
+
+#lobby_reg = {'LBDEF':[["user11"],['user12'],['user13']],'LB4410':[]}
+
+lobby_reg = {'LBDEF':[['111111'],['111']],'LB4410':[['2222222'],['222'],['12345']]}
+n = [['LB4410','111111'],['LB4420','2222222']]
+id = n[1][1]
+'''for i in range(len(n)):
+    if n[i][1] == id:  # проверка гм-айди
+        break
+    for elem in lobby_reg[n[i][0]]:'''
+
+print(*lobby_reg['LBDEF'][0])
+
+
+
+
--- source/repos/MoonliteSoda/Main1f.py
+++ source/repos/MoonliteSoda/Main1f.py
@@ -4,7 +4,8 @@ import telebot
 
 TOKEN = "7578329604:AAEjfLM4OW5MVqPu8k58tN7VfDrWqkeAsJQ"
 
-lobby_reg = {'LBDEF':[],'LB4410':[]}
+lobby_reg = {'LB0000':[['test_user1'],['test_user2']]}
+buffer = []
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -38,8 +39,9 @@ def get_add(message):
         lobby_name = message.text
         user_name = message.chat.id
         if lobby_name in lobby_reg.keys():
-            lobby_reg[lobby_name].append(user_name)
+            lobby_reg[lobby_name].append([user_name])
             bot.send_message(message.chat.id,"Вы подключены!")
+            """добавить только одно возможное лобби в нейминг"""
     else:
         bot.send_message(message.chat.id, "Нет такого лобби.")
 
@@ -47,12 +49,11 @@ def get_add(message):
 @bot.message_handler(commands=['create'])
 def get_creater(message):
     cretor_name = message.chat.id
-    nomer_lob_gen = 'LBDEF'
-    while nomer_lob_gen in lobby_reg.keys():
+    while True:
         nomer_lob_gen = 'LB' + str(randrange(4000,5000))
         if nomer_lob_gen not in lobby_reg.keys():
             break
-    lobby_reg[nomer_lob_gen] = [cretor_name]
+    lobby_reg[nomer_lob_gen] = [[cretor_name]]
     bot.send_message(message.chat.id, f"Создано лобби под номером {nomer_lob_gen}")
 
 
@@ -72,7 +73,18 @@ def print_comands(message):
                                       "/create - создать лобби\n"
                                       "/exit - выйти из лобби\n"
                                       "/mess - написать сообщение(для ГМ)\n"
+                                      "/param - задать параметры ID\n"
                                       "/techData")
+
+
+""""@bot.message_handler(commands=['param'])
+def get_parametru(message):
+    user_name = message.chat.id3."""
+
+
+
+
+
 
 @bot.message_handler(commands=['techData'])
 def info(message):
@@ -86,27 +98,33 @@ def get_message(message):
 
 
 def get_message1(message):
+    lobby_name_1 = message.text
+    buffer.append([lobby_name_1,message.chat.id])
     if message.text in lobby_reg.keys():
-        if lobby_reg[message.text][0] == message.chat.id:
+        a = lobby_reg[lobby_name_1][0]
+        if a == [message.chat.id]:
             bot.send_message(message.chat.id,"Введите текст сообщения")
-            #bot.register_next_step_handler(message, get_message2)
+            bot.register_next_step_handler(message, get_message2)
         else:
             bot.send_message(message.chat.id,"У вас недостаточно прав")
     else:
         bot.send_message(message.chat.id,"Такого лобби не существует")
 
-"""def get_message2(message):
+
+def get_message2(message):
     mes_gm = message.text
-    for lc_id in lobby_reg[lob]"""
-    
-        
+    for i in range(len(buffer)):
+        if buffer[i][1] == message.chat.id:#проверка гм-айди
+            break
+    for elem in lobby_reg[buffer[i][0]]:
+        bot.send_message(*elem,f"{mes_gm}")
+    bot.send_message(message.chat.id,f"Ваше сообщение отправлено")
+    buffer.clear()
 
 
 @bot.message_handler(content_types=['text'])# по факту обязан работать во всех остальных случаях
 def print_help(message):
     bot.send_message(message.chat.id, "Напишите /help для вывода списка команд")
-
-
 
 
 if __name__ == '__main__':
